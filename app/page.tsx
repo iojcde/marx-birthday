1e55c58ecab365957f7cf316c6095fd1aa340d24
Jeeho Ahn <io@jcde.xyz>
2024-10-03 08:50:05 +0900

    f

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,4 @@ export const metadata = {
   title: "혁명의 여명 속 파티 ",
   description:
     "혁명의 사상가 카를 마르크스와 그의 동료 프리드리히 엥겔스가 한 파티에 초대되는데....",
-  opengraph: {
-    images: [
-      "https://flexible.img.hani.co.kr/flexible/normal/500/438/imgdb/original/2021/0527/20210527504154.jpg",
-    ],
-  },
 };
