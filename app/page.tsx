37aada63c34383bd4b1924e55820b353ac3e277e
Jeeho Ahn <io@jcde.xyz>
2024-10-03 08:50:30 +0900

    ㄹ

--- app/page.tsx
+++ app/page.tsx
@@ -8,5 +8,5 @@ export default function Home() {
 export const metadata = {
   title: "혁명의 여명 속 파티 ",
   description:
-    "혁명의 사상가 카를 마르크스와 그의 동료 프리드리히 엥겔스가 한 파티에 초대되는데....",
+    "혁명의 사상가 카를 마르크스와 그의 동료 프리드리히 엥겔스는 한 파티에 초대되는데....",
 };
